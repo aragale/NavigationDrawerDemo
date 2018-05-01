99bc70c88aecb0614a6f2d0b5aa8cc11ecc7ccc0
yuze <asparagale@gmail.com>
2018-05-01 17:55:21 +0800

     定位实现

---
+++ app/src/main/java/com/example/yuze/navigationdrawerdemo/FootPrint.java
@@ -0,0 +1,13 @@
+package com.example.yuze.navigationdrawerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FootPrint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.foot_print);
+    }
+}
--- app/src/main/java/com/example/yuze/navigationdrawerdemo/MainActivity.java
+++ app/src/main/java/com/example/yuze/navigationdrawerdemo/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.yuze.navigationdrawerdemo;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.TextureMapView;
+import com.baidu.mapapi.model.LatLng;
 import com.mindorks.placeholderview.PlaceHolderView;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
 
     LocationClient locationClient;
@@ -20,8 +45,14 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawer;
     private Toolbar mToolbar;
     private PlaceHolderView mGalleryView;
+
     private TextureMapView mMapView;
     private BaiduMap mBaiduMap;
+    private LocationClient mLocationClient;
+    private volatile boolean isFirstLocation = true;//防止每次定位都重新设置中心点和marker
+    private BDLocationListener mListener = new MyLocationListener();
+    private double lat;
+    private double lon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,21 +60,24 @@ public class MainActivity extends AppCompatActivity {
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
-        mMapView = findViewById(R.id.mTexturemap);//地图初始化
-        mBaiduMap = mMapView.getMap();
-
-        mBaiduMap.setMyLocationEnabled(true);//开启定位图层
-        locationClient = new LocationClient(this);
-
-        locationClient.start();
+        mMapView = findViewById(R.id.mTexturemap);
 
         mDrawer = findViewById(R.id.drawerLayout);
         mDrawerView = findViewById(R.id.drawerView);
         mToolbar = findViewById(R.id.toolbar);
         mGalleryView = findViewById(R.id.galleryView);
         setupDrawer();
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            initMap();
+        } else {
+            MainActivityPermissionsDispatcher.ApplySuccessWithPermissionCheck(this);
+        }
     }
 
+    /**
+     * 添加抽屉式导航栏
+     */
     private void setupDrawer() {
         mDrawerView
                 .addView(new DrawerHeader())
@@ -69,19 +103,210 @@ public class MainActivity extends AppCompatActivity {
         drawerToggle.syncState();
     }
 
+    /**
+     * 地图初始化
+     */
+    public void initMap(){
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);//设置地图类型为普通图
+        mMapView.showZoomControls(false);//关闭缩放按钮
+
+        mBaiduMap.setMyLocationEnabled(true);// 开启定位图层
+        mLocationClient = new LocationClient(this);//声明LocationClient类
+        mLocationClient.registerLocationListener(mListener);//注册监听函数
+        initLocation();
+        mLocationClient.start();//开始定位
+    }
+
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        //设置定位模式，高精度，低功耗，仅设备
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+
+        //设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        int span = 5000;
+        option.setScanSpan(span);
+        //可选，默认gcj02，设置返回的定位结果坐标系
+        option.setCoorType("bd09ll");
+        //可选，设置是否需要地址信息，默认不需要
+        option.setIsNeedAddress(true);
+        //可选，默认false,设置是否使用gps
+        option.setOpenGps(true);
+        //可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setLocationNotify(true);
+        //设置是否在stop的时候杀死这个进程，默认不杀死
+        option.setIgnoreKillProcess(false);
+        mLocationClient.setLocOption(option);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: delegate the permission handling to generated method
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    /**
+     * 实现定位监听 位置一旦有所改变就会调用这个方法
+     * 可以在这个方法里面获取到定位之后获取到的一系列数据
+     */
+     public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //获取定位结果
+            location.getTime();    //获取定位时间
+            location.getLocationID();    //获取定位唯一ID，v7.2版本新增，用于排查定位问题
+            location.getLocType();    //获取定位类型
+            location.getLatitude();    //获取纬度信息
+            location.getLongitude();    //获取经度信息
+            location.getRadius();    //获取定位精准度
+            location.getAddrStr();    //获取地址信息
+            location.getCountry();    //获取国家信息
+            location.getCountryCode();    //获取国家码
+            location.getCity();    //获取城市信息
+            location.getCityCode();    //获取城市码
+            location.getDistrict();    //获取区县信息
+            location.getStreet();    //获取街道信息
+            location.getStreetNumber();    //获取街道码
+            location.getLocationDescribe();    //获取当前位置描述信息
+            location.getPoiList();    //获取当前位置周边POI信息
+
+            location.getBuildingID();    //室内精准定位下，获取楼宇ID
+            location.getBuildingName();    //室内精准定位下，获取楼宇名称
+            location.getFloor();    //室内精准定位下，获取当前位置所处的楼层信息
+            //经纬度
+            lat = location.getLatitude();
+            lon = location.getLongitude();
+
+            Log.i("onReceiveLocation", String.format("lat:%f, lon:%f", lat, lon));
+
+
+            //这个判断是为了防止每次定位都重新设置中心点和marker
+            if (isFirstLocation) {
+                isFirstLocation = false;
+                //设置并显示中心点
+                setPosition2Center(mBaiduMap, location, true);
+            }
+        }
+    }
+
+        /**
+         * 设置中心点和添加marker
+         *
+         * @param map
+         * @param bdLocation
+         * @param isShowLoc
+         */
+        public void setPosition2Center(BaiduMap map, BDLocation bdLocation, Boolean isShowLoc) {
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(bdLocation.getRadius())
+                    .direction(bdLocation.getRadius()).latitude(bdLocation.getLatitude())
+                    .longitude(bdLocation.getLongitude()).build();
+            map.setMyLocationData(locData);
+
+            if (isShowLoc) {
+                LatLng ll = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                map.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+        }
+
+    /**
+     * 申请权限成功时
+     */
+    @NeedsPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+    void ApplySuccess() {
+        initMap();
+    }
+
+    /**
+     * 申请权限告诉用户原因时
+     * @param request
+     */
+    @OnShowRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+    void showRationaleForMap(PermissionRequest request) {
+        showRationaleDialog("使用此功能需要打开定位的权限", request);
+    }
+
+    /**
+     * 申请权限被拒绝时
+     *
+     */
+    @OnPermissionDenied(Manifest.permission.ACCESS_COARSE_LOCATION)
+    void onMapDenied() {
+        Toast.makeText(this,"你拒绝了权限，该功能不可用",Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * 申请权限被拒绝并勾选不再提醒时
+     */
+    @OnNeverAskAgain(Manifest.permission.ACCESS_COARSE_LOCATION)
+    void onMapNeverAskAgain() {
+        AskForPermission();
+    }
+
+    /**
+     * 告知用户具体需要权限的原因
+     * @param messageResId
+     * @param request
+     */
+    private void showRationaleDialog(String messageResId, final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.proceed();//请求权限
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(@NonNull DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .setCancelable(false)
+                .setMessage(messageResId)
+                .show();
+    }
+
+    /**
+     * 被拒绝并且不再提醒,提示用户去设置界面重新打开权限
+     */
+    private void AskForPermission() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("当前应用缺少定位权限,请去设置界面打开\n打开之后按两次返回键可回到该应用哦");
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+        builder.setPositiveButton("设置", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.setData(Uri.parse("package:" + MainActivity.this.getPackageName())); // 根据包名打开对应的设置界面
+                startActivity(intent);
+            }
+        });
+        builder.create().show();
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         locationClient.stop();//退出时销毁定位
         mMapView.onDestroy();
         mBaiduMap.setMyLocationEnabled(false);//关闭定位图层
+        mMapView = null;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mMapView.onResume();
-//        sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
